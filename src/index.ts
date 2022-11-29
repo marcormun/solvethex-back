d6b33c30c37521c9c9a02166396202525cc0284e
Marc <mcordontrabajo@gmail.com>
2022-11-29 14:32:00 +0100

    feat: dotenv added, so PORT isn't visible at index.js

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
 import express, { Express } from 'express';
 
+require('dotenv').config();
+
 const app: Express = express();
+
+//middleware
 app.use(express.json());
 
-const PORT = 8000;
+//port
+const PORT = process.env.PORT || 4000;
 
 app.get('/',(req, res) =>{
     res.send('Its working')
 })
 
 app.listen(PORT, () => {
-    console.log("Served started at PORT 8000")
+    console.log("Served started at PORT " + PORT)
 })
