2b45aac5a92b143bb75090698061656fedeb5bfd
Marc <mcordontrabajo@gmail.com>
2022-11-30 13:48:04 +0100

    feat: user model created in models folder

---
+++ src/models/User.ts
@@ -0,0 +1,29 @@
+import { Model, DataTypes } from 'sequelize';
+const sequelize = require('./db/database')
+class User extends Model {};
+
+User.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+},{
+    sequelize,
+    modelName:'user'
+})
+
+module.exports = User;
